a355a93a8a7d84ced3207f7e03edb3cb483e6408
talesmilan <100313945+talesmilan@users.noreply.github.com>
2023-01-09 20:03:22 -0300

    Aplicativo de página unica parte 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap' 
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap' 
+import {Link} from 'react-router-dom'
 
 
     function RenderDish({dish}) {
@@ -20,6 +21,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap'
         return (
             <div className="col-12 col-md-5 m-1">
                 <h2 className="mb-3">Comentários</h2>
+                {console.log(comments)}
                 {comments.map((comment) => {
                 return (<><p>{comment.comment}</p>-- {comment.author}, {comment.date.substr(8, 2)}-{comment.date.substr(5, 2)}-{comment.date.substr(0, 4)}</>)
             })}
@@ -32,8 +34,17 @@ const DishDetail = (props) => {
         return(
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                    </div>
+                </div>
+                <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>)
     } else {
